ae8577069dfd89c45197e3b2689845ef6ddb6204
shasan <shasan@marsdd.com>
2019-08-19 11:34:38 -0400

    New command to add the additional meta fields to the search index (if not yet existing).

--- ckanext/marsavin/commands/marsavin.py
+++ ckanext/marsavin/commands/marsavin.py
@@ -15,7 +15,7 @@ from ckanext.marsavin import migrate
 from ckanext.marsavin.model.package_marsavin import PackageMarsavin
 from ckan.lib.search import rebuild
 
-log = logging.getLogger("ckanext_marsavin")
+log = logging.getLogger("ckanext")
 
 
 class DatabaseCommand(CkanCommand):
@@ -118,3 +118,60 @@ class PackageCommand(CkanCommand):
                     rebuild(package[0].id)
 
             model.repo.commit()
+
+        elif cmd == "update_search_schema":
+            from ckan.lib.search.common import make_connection
+            fields = {
+                "associated_tasks": b'{"add-field":{"name": '
+                                    b'"associated_tasks",  "type": "textgen", '
+                                    b'"indexed": "true", stored: "true"}}',
+                "collection_period": b'{"add-field":{"name": '
+                                    b'"collection_period",  "type": "textgen", '
+                                    b'"indexed": "true", stored: "true"}}',
+                "geographical_area": b'{"add-field":{"name": '
+                                    b'"geographical_area",  "type": "textgen", '
+                                    b'"indexed": "true", stored: "true"}}',
+                "number_of_instances": b'{"add-field":{"name": '
+                                    b'"number_of_instances",  '
+                                       b'"type": "textgen", '
+                                    b'"indexed": "true", stored: "true"}}',
+                "number_of_attributes": b'{"add-field":{"name": '
+                                    b'"number_of_attributes",  '
+                                        b'"type": "textgen", '
+                                    b'"indexed": "true", stored: "true"}}',
+                "pkg_description": b'{"add-field":{"name": '
+                                    b'"pkg_description",  "type": "textgen", '
+                                    b'"indexed": "true", stored: "true"}}',
+                "creation_date": b'{"add-field":{"name": '
+                                    b'"creation_date",  "type": "date", '
+                                    b'"indexed": "true", stored: "true"}}',
+                "expiry_date": b'{"add-field":{"name": '
+                                    b'"expiry_date",  "type": "date", '
+                                    b'"indexed": "true", stored: "true"}}',
+                "has_missing_values": b'{"add-field":{"name": '
+                                    b'"has_missing_values",  '
+                                      b'"type": "boolean", '
+                                    b'"indexed": "true", stored: "true"}}',
+            }
+
+            copy_fields = {
+                "associated_tasks": b'{"add-copy-field":{"source": '
+                                    b'"associated_tasks",  "dest": "text"}}',
+                "collection_period": b'{"add-copy-field":{"source": '
+                                    b'"collection_period",  "dest": "text"}}',
+                "geographical_area": b'{"add-copy-field":{"source": '
+                                    b'"geographical_area",  "dest": "text"}}',
+                "pkg_description": b'{"add-copy-field":{"source": '
+                                    b'"pkg_description",  "dest": "text"}}'
+            }
+
+            conn = make_connection()
+            path = "schema"
+            for fieldname in fields:
+                res = conn._send_request("post", path, fields[fieldname])
+                log.debug("Result of update {result}".format(result=res))
+
+            for fieldname in copy_fields:
+                res = conn._send_request("post", path, copy_fields[fieldname])
+                log.debug("Result of update {result}".format(result=res))
+            pass
