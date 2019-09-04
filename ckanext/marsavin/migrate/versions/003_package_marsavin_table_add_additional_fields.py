1fcc9acb1658181dc390305eaa4b1ee06a4043d2
shasan <shasan@marsdd.com>
2019-09-04 10:06:37 -0400

    Fixing bug with migrate added if not exists functions.

--- ckanext/marsavin/migrate/versions/003_package_marsavin_table_add_additional_fields.py
+++ ckanext/marsavin/migrate/versions/003_package_marsavin_table_add_additional_fields.py
@@ -4,10 +4,10 @@
 def upgrade(migrate_engine):
     migrate_engine.execute(
         '''
-        ALTER TABLE public.package_marsavin add column number_of_attributes text;
-        ALTER TABLE public.package_marsavin add column creation_date date;
-        ALTER TABLE public.package_marsavin add column expiry_date date;
-        ALTER TABLE public.package_marsavin add column has_missing_values boolean;
-        ALTER TABLE public.package_marsavin ADD CONSTRAINT package_id_unique unique (package_id);
+        ALTER TABLE public.package_marsavin add column if not exists number_of_attributes text;
+        ALTER TABLE public.package_marsavin add column if not exists creation_date date;
+        ALTER TABLE public.package_marsavin add column if not exists expiry_date date;
+        ALTER TABLE public.package_marsavin add column if not exists has_missing_values boolean;
+        ALTER TABLE public.package_marsavin ADD CONSTRAINT if not exists package_id_unique unique (package_id);
         '''
     )
