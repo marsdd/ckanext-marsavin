ed4654cfd1089c76bdbec10c0f846333efe2051c
shasan <shasan@marsdd.com>
2019-10-01 16:27:19 -0400

    Created webp versions of the hero image to save some decent bandwidth for browsers that support it.

--- ckanext/marsavin/plugin.py
+++ ckanext/marsavin/plugin.py
@@ -40,7 +40,8 @@ class MarsavinPlugin(plugins.SingletonPlugin, DefaultTranslation,
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
-        toolkit.add_resource('public', 'marsavin')
+        toolkit.add_resource('public/javascript', 'marsavin')
+        toolkit.add_resource('public/css', 'marsavin')
         config_['ckan.favicon'] = "/images/avin.ico"
 
     # IBlueprint
