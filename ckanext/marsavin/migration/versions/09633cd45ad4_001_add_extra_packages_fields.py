3382fad33107dfe88069d939fa16e99da34ebf65
shasan <shasan@marsdd.com>
2019-12-06 10:37:44 -0500

    Working on the alembic migration.

--- ckanext/marsavin/migration/versions/09633cd45ad4_001_add_extra_packages_fields.py
+++ ckanext/marsavin/migration/versions/09633cd45ad4_001_add_extra_packages_fields.py
@@ -17,9 +17,22 @@ depends_on = None
 
 
 def upgrade():
-    
+    with op.batch_alter_table("package") as batch_op:
+        batch_op.add_column(sa.Column("associated_tasks", sa.Text()))
+        batch_op.add_column(sa.Column("collection_period", sa.Text()))
+        batch_op.add_column(sa.Column("geographical_area", sa.Text()))
+        batch_op.add_column(sa.Column("number_of_instances", sa.Text()))
+        batch_op.add_column(sa.Column("number_of_missing_values", sa.Text()))
+        batch_op.add_column(sa.Column("pkg_description", sa.Text()))
     pass
 
 
 def downgrade():
+    with op.batch_alter_table("package") as batch_op:
+        batch_op.drop_column("associated_tasks")
+        batch_op.drop_column("collection_period")
+        batch_op.drop_column("geographical_area")
+        batch_op.drop_column("number_of_instances")
+        batch_op.drop_column("number_of_missing_values")
+        batch_op.drop_column("pkg_description")
     pass
--- ckanext/marsavin/migration/versions/adc183cca9c4_002_access_request_table_plus_marsavin_.py
+++ ckanext/marsavin/migration/versions/adc183cca9c4_002_access_request_table_plus_marsavin_.py
@@ -7,6 +7,7 @@ Create Date: 2019-11-25 10:47:30.619605
 """
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
 
 
 # revision identifiers, used by Alembic.
@@ -17,8 +18,21 @@ depends_on = None
 
 
 def upgrade():
+    op.execute('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
+    with op.batch_alter_table("package_marsavin") as batch_op:
+        batch_op.add_column(sa.Column('id', postgresql.UUID(),
+                            server_default=sa.text('uuid_generate_v4')))
+        batch_op.add_column(sa.Column('package_id', sa.Text))
+        batch_op.add_column(sa.Column('associated_tasks', sa.Text))
+        batch_op.add_column(sa.Column('associated_tasks', sa.Text))
+        batch_op.add_column(sa.Column('collection_period', sa.Text))
+        batch_op.add_column(sa.Column('geographical_area', sa.Text))
+        batch_op.add_column(sa.Column('number_of_instances', sa.Text))
+        batch_op.add_column(sa.Column('number_of_missing_values', sa.Text))
+        batch_op.add_column(sa.Column('pkg_description', sa.Text))
     pass
 
 
 def downgrade():
+    op.drop_table("package_marsavin")
     pass
