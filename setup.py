78bf0c3d3813b090b0d12bf011936d462525b9e0
shasan <shasan@marsdd.com>
2020-09-09 16:34:42 -0400

    removed old paster commands.

--- setup.py
+++ setup.py
@@ -87,10 +87,6 @@ setup(
 
         [babel.extractors]
         ckan = ckan.lib.extract:extract_ckan
-        
-        [paste.paster_command]
-        db = ckanext.marsavin.commands.marsavin:DatabaseCommand
-        package = ckanext.marsavin.commands.marsavin:PackageCommand
     ''',
 
     # If you are changing from the default layout of your extension, you may
