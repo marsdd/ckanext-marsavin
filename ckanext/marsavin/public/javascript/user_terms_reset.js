7cee7f320b6fba4bfc4bf9f807612f5faa2ecbe5
shasan <shasan@marsdd.com>
2020-10-05 17:33:16 -0400

    Updating the agreement javascript validation to add additional check before enabling submit UI button.

--- ckanext/marsavin/public/javascript/user_terms_reset.js
+++ ckanext/marsavin/public/javascript/user_terms_reset.js
@@ -4,11 +4,11 @@ this.ckan.module('user-terms-reset', function(jQuery, _) {
     options: {
     },
     initialize: function() {
-      jQuery('#field-user-terms-agree').change(function() {
-          if(this.checked) {
-              jQuery(".form-actions").find("button.btn-primary").attr("disabled", false);
+      jQuery('#field-user-terms-agree, #field-uploader-terms-agree').change(function() {
+          if(jQuery("#field-user-terms-agree").is(":checked") && jQuery("#field-uploader-terms-agree").is(":checked")) {
+            jQuery(".form-actions").find("button.btn-primary").attr("disabled", false);
           } else {
-              jQuery(".form-actions").find("button.btn-primary").attr("disabled", true);
+            jQuery(".form-actions").find("button.btn-primary").attr("disabled", true);
           }
       });
       jQuery(".form-actions").find("button.btn-primary").attr("disabled", true);
